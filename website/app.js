f636533252bc631b8ca2b683c61e2a13ffd8ffba
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-05 20:10:25 +0000

    Add fetchProjectData async arrow func to website/app.js

--- website/app.js
+++ website/app.js
@@ -3,3 +3,17 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// fetch project data
+const fetchProjectData = async () => {
+  try {
+    const response = await fetch("/api/project-data");
+    const body = await response.json();
+    console.log("fetchProjectData, success: " + JSON.stringify(body));
+    return body;
+  } catch (error) {
+    console.log("fetchProjectData, failure: " + error);
+  };
+};
+
+fetchProjectData();
